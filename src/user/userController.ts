d3aa4f0eb5d4289155f34d436f15bf32fbd6156e
AnujGahra <abujgahra244304@gmail.com>
2025-02-13 16:22:37 +0530

    add login endpoint

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -5,53 +5,101 @@ import User from "./userModal";
 import { sign } from "jsonwebtoken";
 import { config } from "../config/config";
 
-export const createUser = async (req: Request, res: Response, next: NextFunction) => {
+// Bussiness Logic for Create User
+export const createUser = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
     const { name, email, password } = req.body;
-  
+
     // Validation
     if (!name || !email || !password) {
-      const error = createHttpError(400, "All fields are required");
-      return next(error);
+        const error = createHttpError(400, "All fields are required");
+        return next(error);
     }
-  
+
     // Database call.
     try {
-      const user = await User.findOne({ email });
-      if (user) {
-        const error = createHttpError(
-          400,
-          "User already exists with this email."
-        );
-        return next(error);
-      }
-    } catch (err) {
-      return next(createHttpError(500, "Error while getting user"));
+        const user = await User.findOne({ email });
+        if (user) {
+            const error = createHttpError(
+                400,
+                "User already exists with this email."
+            );
+            return next(error);
+        }
+    } catch (error) {
+        console.error("Error during register:", error);
+        return next(createHttpError(500, "Error while getting user"));
     }
-  
+
     /// password -> hash
-  
+
     const hashedPassword = await bcrypt.hash(password, 10);
-  
+
     let newUser: User;
     try {
-      newUser = await User.create({
-        name,
-        email,
-        password: hashedPassword,
-      });
-    } catch (err) {
-      return next(createHttpError(500, "Error while creating user."));
+        newUser = await User.create({
+            name,
+            email,
+            password: hashedPassword,
+        });
+    } catch (error) {
+        console.error("Error during create user:", error);
+        return next(createHttpError(500, "Error while creating user."));
     }
-  
+
     try {
-      // Token generation JWT
-      const token = sign({ sub: newUser._id }, config.jwtSeceret as string, {
-        expiresIn: "7d",
-        algorithm: "HS256",
-      });
-      // Response
-      res.status(201).json({ accessToken: token });
-    } catch (err) {
-      return next(createHttpError(500, "Error while signing the jwt token",));
+        // Token generation JWT
+        const token = sign({ sub: newUser._id }, config.jwtSecret as string, {
+            expiresIn: "7d",
+            algorithm: "HS256",
+        });
+        // Response
+        res.status(201).json({ accessToken: token });
+    } catch (error) {
+        console.error("Error during register:", error);
+        return next(createHttpError(500, "Error while signing the jwt token"));
     }
-  };
+};
+
+// Bussiness Logic for Login User
+export const loginUser = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    try {
+        const { email, password } = req.body;
+
+        if (!email || !password) {
+            return next(createHttpError(400, "All fields are required"));
+        }
+
+        const user = await User.findOne({ email });
+        if (!user) {
+            return next(createHttpError(404, "User not found."));
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return next(createHttpError(400, "Username or password incorrect!"));
+        }
+
+        // Create access token
+        const token = sign({ sub: user._id }, config.jwtSecret as string, {
+            expiresIn: "7d",
+            algorithm: "HS256",
+        });
+
+        res.json({ accessToken: token });
+    } catch (error) {
+        // Log the error for debugging
+        console.error("Error during login:", error);
+
+        // Pass the error to the error-handling middleware
+        next(createHttpError(500, "An internal server error occurred"));
+    }
+};
+
